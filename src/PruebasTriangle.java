43d494816e766a51d992b3e81428ee4f287371b6
JacomeRodriguez <willjey.jacome7@gmail.com>
2020-06-12 20:42:51 -0500

    Failed Test
    Failed Test

--- src/PruebasTriangle.java
+++ src/PruebasTriangle.java
@@ -21,7 +21,7 @@ public class PruebasTriangle {
 		 //c4. a <= b + c
 		 //c6. c <= a + b
 		
-		// => 1 , 5 , 7 donde  7!<= 1 + 5 (no se cumple la condición 6)
+		// => 1 , 5 , 7 donde  7!<= 1 + 5 (no se cumple la condiciï¿½n 6)
 		System.out.println("Test 1: "+ "Not a Triangle".equals(triangleType.tipoTriangulo("1","5","7")));
 		
 		// Escaleno => 7,9,10 
@@ -33,7 +33,21 @@ public class PruebasTriangle {
 		// Escaleno => 10,10,10
 		System.out.println("Test 4: "+ "Equilateral".equals(triangleType.tipoTriangulo("10","10","10")));
 		
+		// Failed Test 1: Use of Strings
+		System.out.println("Test 5: "+ "Scalene".equals(triangleType.tipoTriangulo("10","s","s")));
+		System.out.println("Test 5: "+ "Isosceles".equals(triangleType.tipoTriangulo("10","s","s")));
+		System.out.println("Test 5: "+ "Equilateral".equals(triangleType.tipoTriangulo("10","s","s")));
 				
+		// Failed Test 2: Use of decimal
+		System.out.println("Test 6: "+ "Scalene".equals(triangleType.tipoTriangulo("7.5","9","10")));
+		System.out.println("Test 6: "+ "Isosceles".equals(triangleType.tipoTriangulo("7.5","7","10")));
+		System.out.println("Test 6: "+ "Equilateral".equals(triangleType.tipoTriangulo("10.5","10","10")));
+		
+		// Failed Test 3: Out of range
+		System.out.println("Test 7: "+ "Scalene".equals(triangleType.tipoTriangulo("7","9","201")));
+		System.out.println("Test 7: "+ "Isosceles".equals(triangleType.tipoTriangulo("7","7","201")));
+		System.out.println("Test 7: "+ "Equilateral".equals(triangleType.tipoTriangulo("201","201","201")));
+		
 		
 	}
 
